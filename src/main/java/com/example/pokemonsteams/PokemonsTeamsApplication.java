2cbdcec2b113dcde4c35a8e9bb5b2a49383a241a
Arthur Frossard <frossard65@gmail.com>
2024-03-04 20:12:29 -0300

    First commit

---
+++ src/main/java/com/example/pokemonsteams/PokemonsTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonsteams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonsTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonsTeamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemonsteams/PokemonsTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonsteams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonsTeamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
